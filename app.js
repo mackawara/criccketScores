f4616b6b75f35054d28a47f4c747da3a67f25976
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-07 19:55:15 -0400

    added ejs view engine

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const ejs =require("ejs")
+
+
 const PORT = process.env.PORT || 5007;
 //const fetch = import("node-fetch");
 const { json } = require("express/lib/response");
@@ -10,7 +13,20 @@ const mongoose=require("mongoose")
 const mongodb=require("mongodb")
 const mongooseURL=process.env.MONGOOSEURL
 
-mongoose.Connection()
+//mongoose.Connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mkawara:<password>@hand2hand.4pnao.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+/// Middleware
+app.set("view-engine", ejs)
+
 
 app.listen(PORT, () => {
   console.log("Server listenting on port " + PORT);
