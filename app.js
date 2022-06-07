b06b249a3c0793a23cd9d59a20036b554ae881a0
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-07 16:43:20 +0200

    removed rapid API links

--- app.js
+++ app.js
@@ -4,31 +4,10 @@ const dotenv = require("dotenv");
 const PORT = process.env.PORT || 5007;
 //const fetch = import("node-fetch");
 const { json } = require("express/lib/response");
-const rapidapiKEY = process.env.RAPIDAPIKEY;
-const rapidapiHOST = process.env.RAPIDAPIHOST;
-
-const circUrl = "https://cricket-live-scores4.p.rapidapi.com/api";
-
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Host": rapidapiHOST,
-    "X-RapidAPI-Key": rapidapiKEY,
-  },
-};
-
-fetch(circUrl, options)
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error("error:" + err));
 
 app.listen(PORT, () => {
   console.log("Server listenting on port " + PORT);
 });
 app.get("/", (req, res) => {
-  fetch(circUrl, options)
-    .then((res) => res.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.error("error:" + err));
   res.sendStatus("200").send(`${json}`);
 });
