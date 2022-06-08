6f196c5d590796048342fd1f07e7d27144868954
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-07 20:16:52 -0400

    added cricket dat link and mongodb code

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const ejs =require("ejs")
-
+const ejs = require("ejs");
 
 const PORT = process.env.PORT || 5007;
 //const fetch = import("node-fetch");
 const { json } = require("express/lib/response");
 
 //Mongds/mongoose setup
-const mongoose=require("mongoose")
-const mongodb=require("mongodb")
-const mongooseURL=process.env.MONGOOSEURL
+const mongoose = require("mongoose");
+const mongodb = require("mongodb");
+const mongooseURL = process.env.MONGOOSEURL;
+const fetchFromOffset = require("./cricketdata");
 
 //mongoose.Connection
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mkawara:<password>@hand2hand.4pnao.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://mkawara:<password>@hand2hand.4pnao.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });
 
 /// Middleware
-app.set("view-engine", ejs)
-
+app.set("view engine", `ejs`);
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log("Server listenting on port " + PORT);
 });
 app.get("/", (req, res) => {
-  res.sendStatus("200").send(`${json}`);
+  fetchFromOffset(0)
+    .then(function (data) {
+      res.send(data);
+    })
+    .catch((e) => console.log);
 });
